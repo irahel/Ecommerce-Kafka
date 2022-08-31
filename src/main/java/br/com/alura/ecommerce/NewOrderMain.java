735e6eaf96eb50156165057ecac5fb1a1a6f1caf
Isaac Rahel <isaac_rahel@live.com>
2022-08-31 18:38:50 -0300

    refactor: extract producer

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,49 +1,22 @@
 package br.com.alura.ecommerce;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
-
-import java.util.Properties;
+import java.io.IOException;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var producer = new KafkaProducer<String, String>(properties());
-        for(var i = 0; i < 10; i++){
-            var key = UUID.randomUUID().toString();
-            var value = "ID1313, 37.00, 1212";
-            var record = new ProducerRecord<>("ECOMMERCE_NEWORDER", key, value);
-            Callback callback = (data, ex) -> {
-                        if(ex != null){
-                            ex.printStackTrace();
-                            return;
-                        }
-                        System.out.println("success:::" + data.topic() + ":::" +data.partition() + "/" + data.offset() + "/" + data.timestamp());
-                    };
-            
-            var email = "Thank you for your order! We are processing your order!";
-            var emailRecord = new ProducerRecord<>("ECOMMERCE_SENDEMAIL", key, email);
-            producer.send(record, callback).get();        
-            producer.send(emailRecord, callback).get();
-        }
-        
+    public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
+        try (var dispatcher = new KafkaDispatcher()) {
+            for(var i = 0; i < 10; i++){
+                var key = UUID.randomUUID().toString();
+                var value = key + ", 37.00, 1212";
+                
+                dispatcher.send("ECOMMERCE_NEWORDER", key, value);
+
+                var email = "Thank you for your order! We are processing your order!";            
+                dispatcher.send("ECOMMERCE_SENDEMAIL", key, email);
+            }
+        }        
     }
-
-    private static Properties properties() {
-        var properties = new Properties();
-
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
-        return properties;
-    }
-
 }
-
-
